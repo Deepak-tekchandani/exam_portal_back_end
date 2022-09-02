03f4939895789d6ce52685fc674954b919e117bf
deepu <deepaktekchandani143@gmail.com>
2022-09-02 16:02:22 +0500

    added the @CrossOrigin annotation in user controller

--- src/main/java/com/exam/portal/controller/UserController.java
+++ src/main/java/com/exam/portal/controller/UserController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/user")
 public class UserController {
 
@@ -19,7 +20,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/")
-    public UserEntity create(@ModelAttribute UserEntity userEntity) throws Exception {
+    public UserEntity create( UserEntity userEntity) throws Exception {
 
         Set<UserRoleEntity> roles = new HashSet<>();
         RoleEntity role = new RoleEntity();
